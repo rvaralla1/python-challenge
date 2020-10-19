4047fcf247406eea08bdb0226bd9a723d3900749
rvaralla1 <rvaralla1@gmail.com>
2020-10-19 14:01:05 -0400

    update 1

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,64 +2,63 @@ import os
 import csv
 
 bank_csv = os.path.join('Resources', 'budget_data.csv')
-
+file_to_output = os.path.join("budget_analysis.txt")
 with open(bank_csv, 'r') as csvfile:
 
     csvreader = csv.reader(csvfile)
 
     header = next(csvreader)
-    first_row = next(csvreader)
-    x = first_row[1]
+    
     
 
     total_months=[]
     total_dollars=[]
     profit=[]
-    initial_profit=[]
+    #initial_profit=[]
     pandl=[]
     pandl_list=[]
     total_profit=[]
-    greatest_increase_in_profits=[]
-    greatest_decrease_in_profits=[]
+    greatest_increase_in_profits=["", 9999]
+    greatest_decrease_in_profits=["",0]
 
-    initial_profit = x
-
+    initial_profit = 0
+    
     for row in csvreader:
         total_months.append(row[0])
+        
         total_dollars.append(int(row[1]))
-
-        initial_profit = int(profit[x])
-
-        pandl = (initial_profit - int(profit[x-1]))
+       
+        pandl = int(row[1]) - initial_profit
         pandl_list.append(pandl)
+        initial_profit=int(row[1])
+        length = len(pandl_list)-1
 
-        total_profit = sum(pandl_list)
+        if pandl > greatest_increase_in_profits[1]:
+            greatest_increase_in_profits[0] = row[0]
+            greatest_increase_in_profits[1] = pandl
 
-        average_change = total_dollars / len(total_months)
+        if pandl < greatest_decrease_in_profits[1]:
+            greatest_decrease_in_profits[0] = row[0]
+            greatest_decrease_in_profits[1] = pandl  
+    month_count = len(total_months)  
+    amount = sum(total_dollars)
+    total_change = sum(pandl_list[1:])   
 
-        greatest_increase_in_profits = max([1])
-        greatest_increase_month = pandl.index(greatest_increase_in_profits)
+    average_change = total_change /length
 
-        greatest_decrease_in_profits = min([1])
-        greatest_decrease_month = pandl.index(greatest_decrease_in_profits)
 
-        print(f"Financial Analysis")
-        print("--------------------")
-        print(f"Total Months:{len(total_months)}")
-        print(f"Total:{sum(total_dollars)}")
-        print(f"Average Change:{average_change}")
-        print(f"Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase_in_profits})")
-        print(f"Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease_in_profits})")
-
-f= open("financial.txt","w+")
-for i in range(7):
-    f.write(
-
-        (f"Financial Analysis")
-        (f"Total Months:{len(total_months)}")
-        (f"Total:{sum(total_dollars)}")
-        (f"Average Change:{average_change}")
-        (f"Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase_in_profits})")
-        (f"Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease_in_profits})")
-    )
-    f.close()
+    
+    print(f"Financial Analysis")
+    print("--------------------")
+    print(f"Total Months:{month_count}")
+    print(f"Total:{amount}")
+    # print(pandl_list)
+    print(f"Average Change:{average_change}")
+    print(f"Greatest Increase in Profits: {greatest_increase_in_profits[0]} (${greatest_increase_in_profits[1]})")
+    print(f"Greatest Decrease in Profits: {greatest_decrease_in_profits[0]} (${greatest_decrease_in_profits[1]})")
+with open(file_to_output, "w") as txt_file:
+    txt_file.write(f'Total Months: $ {month_count}\n' ) 
+    txt_file.write(f'Total Sum: $ {amount}\n' )
+    txt_file.write(f"Average Change:{average_change}\n")
+    txt_file.write(f"Greatest Increase in Profits: {greatest_increase_in_profits[0]} (${greatest_increase_in_profits[1]})\n")
+    txt_file.write(f"Greatest Decrease in Profits: {greatest_decrease_in_profits[0]} (${greatest_decrease_in_profits[1]})\n")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,37 +6,52 @@ poll_csv = 'Resources/election_data.csv'
 with open(poll_csv, 'r') as csvfile:
 
     csvreader = csv.reader(csvfile)
-
-    total_votes=[]
-    winner=[]
-    top_results=[]
-
-
+    header = next(csvreader)
+    total_votes=0
+    winner=""
+    top_results=0
+    candidate_list=[]
+    candidate_dict={}
+    percent_of={}
     for row in csvreader:
-        voter_id = int(poll_csv[0])
+       # voter_id = int(poll_csv[0])
         county = str(poll_csv[1])
-        candidate = str(poll_csv[2])
+        candidate = (row[2])
 
 
-        count = 0
-        total = 0
-        total_votes = len(poll_csv[0])
+        # count = 0
+        # total = 0
+        total_votes = total_votes+1
+        if candidate not in candidate_list:
+            candidate_list.append(candidate)
+            candidate_dict[candidate]=0
+        candidate_dict[candidate]=candidate_dict[candidate]+1
+    print(f"Election Results")
+    print("--------------------")
+    print(f"Total Votes:{total_votes}")
+    print("--------------------")
+    print(candidate_list)
+    print("--------------------")
+    for x in candidate_dict:
+        percent_of[x]=round(candidate_dict[x]/total_votes*100,2)
+        if candidate_dict[x]>top_results:
+            top_results=candidate_dict[x]
+            winner=x
+        print(f":{x}: {percent_of[x]}% ({candidate_dict[x]})")
         
 
-        print(f"Election Results")
-        print("--------------------")
-        print(f"Total Votes:{total_votes}")
-        print("--------------------")
-        print({top_results1})
-        print("--------------------")
-        print(f"Winner:{winning_candidate}")
+    
+    #print(f":{percent_of}{candidate_dict[x]}")
+    #print(percent_of)
+    print("--------------------")
+    print(f"Winner:{winner}")
 
-f= open("financial.txt","w+")
-for i in range(7):
-    f.write(
-        (f"Election Results")
-        (f"Total Votes:{total_votes}")
-        ({top_results1})
-        (f"Winner:{winning_candidate}")
+with open("Election.txt","w+") as f:
+
+    f.write(f"Election Results \n")
+    f.write(f"Total Votes:{total_votes}\n")
+    f.write(f"{candidate_list}\n")
+    f.write(f"{percent_of}\n")
+    f.write(f"Winner: {winner}")
     f.close()
     
